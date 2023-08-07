d97c873d713cc4e22cf6b3165fe7680385666863
kawsar-pie <79654190+kawsar-pie@users.noreply.github.com>
2023-08-07 15:54:20 +0600

    neural network changed

--- Neural_Network/DNN_example.py
+++ Neural_Network/DNN_example.py
@@ -1,7 +1,21 @@
 from neural_net import Neural_Net
 import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-X = np.random.randint(0, 2, (2, 30))
-Y = np.logical_and(X[0] == 0, X[1] == 1).astype(int).reshape((1, 30))
+X, y = make_classification(n_samples=10000)
+# print("X Y")
+# print(X.shape,y.shape)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, random_state=42, test_size=0.2)
+# print(X_train.shape, X_test.shape, y_train.shape, y_test.shape )
 dnn = Neural_Net()
-dnn.nn_model(X, Y, num_iterations=50, learning_rate=1.2)
+dnn.nn_model(X_train, y_train, num_iterations=100, learning_rate=0.0001)
+
+y_pred = dnn.predict(X_test)
+y_pred =  y_pred.reshape(y_pred.shape[0],)
+# print(X_test)
+# print(y_test)
+# print(y_pred.shape, y_test.shape)
+print(accuracy_score(y_test, y_pred))
--- Neural_Network/neural_net.py
+++ Neural_Network/neural_net.py
@@ -4,46 +4,52 @@ import numpy as np
 class Neural_Net:
 
     def __init__(self):
-        self.parameters = {}
+        self.parameters = {"W": 0, "b": 0}
         self.grads = {}
 
     def sigmoid(self, z):
         return 1/(1 + np.exp(-z))
 
-    def initialize_parameters(self, n_x, n_y):
-        W = np.random.randn(n_y, n_x) * 0.01
-        b = np.zeros((n_y, 1))
+    def initialize_parameters(self, no_samples, no_features):
+        self.parameters["W"] = np.zeros((no_features, 1))
+        self.parameters["b"] = 0
+        # print("initialize_parameters")
+        # print(self.parameters["W"].shape)
 
-        self.parameters = {"W": W,
-                           "b": b}
+        # self.parameters = {"W": W,
+        #                    "b": b}
 
     def forward_propagation(self, X):
         W = self.parameters["W"]
         b = self.parameters["b"]
-
-        Z = np.matmul(W, X) + b
+        # print("forward_propagation")
+        # print(X.shape, W.shape)
+        Z = np.matmul(X, W) + b
         A = self.sigmoid(Z)
 
         return A
 
     def compute_cost(self, A, Y):
-        # Number of examples.
-        m = Y.shape[1]
-
-        # Compute the cost function.
-        logprobs = - np.multiply(np.log(A), Y) - \
-            np.multiply(np.log(1 - A), 1 - Y)
-        cost = 1/m * np.sum(logprobs)
+        m = Y.shape[0]
+        cost = np.square(np.subtract(A, Y)).mean()
 
         return cost
 
     def backward_propagation(self, A, X, Y):
-
-        m = X.shape[1]
-
+        m = X.shape[0]
+        Y = Y.reshape(m, 1)
         dZ = A - Y
-        dW = 1/m * np.dot(dZ, X.T)
-        db = 1/m * np.sum(dZ, axis=1, keepdims=True)
+        dW = 1/m * np.dot(X.T, dZ)
+        db = 1/m * np.sum(dZ, axis=0, keepdims=True)
+
+        # Print shapes for debugging
+        # print("backward_propagation")
+        # print("dW", dW.shape)
+        # print("X", X.shape)
+        # print("dZ", dZ.shape)
+        # print("A", A.shape)
+        # print("Y", Y.shape)
+        # print("db", db.shape)
 
         self.grads = {"dW": dW,
                       "db": db}
@@ -55,7 +61,6 @@ class Neural_Net:
 
         dW = self.grads["dW"]
         db = self.grads["db"]
-
         W = W - learning_rate * dW
         b = b - learning_rate * db
 
@@ -63,28 +68,19 @@ class Neural_Net:
                            "b": b}
 
     def nn_model(self, X, Y, num_iterations=10, learning_rate=1.2):
+        no_features = X.shape[1]
+        no_samples = X.shape[0]
+        Y = Y.reshape(no_samples, 1)
+        self.initialize_parameters(no_samples, no_features)
 
-        n_x = X.shape[0]
-        n_y = Y.shape[0]
-
-        self.initialize_parameters(n_x, n_y)
-
-        for i in range(0, num_iterations):
-
+        for i in range(num_iterations):
             A = self.forward_propagation(X)
-
             cost = self.compute_cost(A, Y)
-
             self.backward_propagation(A, X, Y)
-
             self.update_parameters(learning_rate)
-
             print("Cost after iteration %i: %f" % (i, cost))
 
     def predict(self, X):
-
         A = self.forward_propagation(X)
-        predictions = A > 0.5
-
+        predictions = (A > 0.5).astype(int)
         return predictions
-    
